365d1d0bf9a9129c1131de64325574902659c7fa
やはゆた <13937582+yahayuta@users.noreply.github.com>
2024-04-10 17:45:39 +0900

    update comments

--- model_chat_log.py
+++ model_chat_log.py
@@ -1,12 +1,12 @@
 from google.cloud import bigquery
 
-# チャットログ保存
+# save chat log
 def save_log(role, msg):
     client = bigquery.Client()
     query_job = client.query(f'INSERT INTO app.chat_log(user_id,message,role,created) values(\'mate\',\'\'\'{msg}\'\'\',\'{role}\',CURRENT_DATETIME(\'Asia/Tokyo\'))')
     print(query_job)
     
-# チャットログロード
+# load chat logs
 def get_logs():
     client = bigquery.Client()
     query_job = client.query("SELECT * FROM app.chat_log WHERE user_id = 'mate' order by created")
@@ -18,7 +18,7 @@ def get_logs():
         logs.append(log)
     return logs
 
-# チャットログ削除
+# delete chat logs
 def delete_logs():
     client = bigquery.Client()
     query_job = client.query("DELETE FROM app.chat_log WHERE user_id = 'mate'")
